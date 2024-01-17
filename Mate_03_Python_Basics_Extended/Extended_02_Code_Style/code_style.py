179524f91bdbb2cca5abcf67764e493d400d41d2
Artem Andriushchenko <link.by.artem@gmail.com>
2024-01-18 01:45:20 +0200

    add code_style.py

---
+++ Mate_03_Python_Basics_Extended/Extended_02_Code_Style/code_style.py
@@ -0,0 +1,46 @@
+"""
+Python has a widely accepted style guide called PEP 8 (Python Enhancement Proposal 8)
+which provides conventions on how to format Python code for maximum readability.
+Adhering to a consistent coding style makes your code more maintainable and helps collaboration
+with other developers.
+
+Here are some key points from PEP 8:
+
+1. Indentation
+2. Maximum Line Length
+3. Imports
+4. Whitespace in Expressions and Statements
+5. Comments
+6. Naming Conventions
+7. Function and Method Arguments
+
+
+1. Indentation:
+Use 4 spaces per indentation level. Never use tabs.
+
+2. Maximum Line Length:
+Limit all lines to a maximum of 79 characters for code (72 for docstrings and comments).
+
+3. Imports:
+Imports should usually be on separate lines. Wildcard imports (from module import *)
+should be avoided.
+
+4. Whitespace in Expressions and Statements
+Avoid extraneous whitespace spaces inside brackets and before/after comparison operators.
+
+5. Comments:
+Comments should be complete sentences and placed on a line of their own if possible.
+
+6. Naming Conventions:
+Follow the naming conventions outlined in PEP 8.
+In general, use lowercase for variable and function names, and use underscores to separate words.
+
+7. Function and Method Arguments:
+Don't put spaces around the = sign when used to indicate a keyword argument or
+a default parameter value.
+
+Adhering to PEP 8 helps maintain a clean and consistent code base.
+You can use integrate linters in your development environment to automatically format your code
+according to PEP 8.
+
+"""
