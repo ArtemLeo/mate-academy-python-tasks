def8ca88128d08d4f90a2896c47973828567e006
Artem Andriushchenko <link.by.artem@gmail.com>
2024-01-18 02:04:43 +0200

    add working_with_numbers.py

---
+++ Mate_03_Python_Basics_Extended/Extended_04_Working_With_Numbers/working_with_numbers.py
@@ -0,0 +1,49 @@
+"""
+Working with numbers in Python involves performing various mathematical operations,
+using built-in functions, and leveraging the capabilities of numeric data types.
+Here's a brief overview:
+
+1. Basic Arithmetic Operations:
+Python supports standard arithmetic operations for numbers:
+
+# Addition +
+# Subtraction  -
+# Multiplication *
+# Division /
+# Exponentiation **
+
+2. Numeric Data Types:
+Python has several numeric data types. Being the most common:
+
+# int (integer)
+# float (floating-point)
+
+3. Type Conversion:
+You can convert between different numeric types using functions like int(), float(), and str().
+
+4. Math Module (import math):
+
+# Square root -> sqrt_result = math.sqrt(25)
+# Trigonometric -> sin_result = math.sin(math.radians(30)), cos_result = math.cos(math.radians(60))
+# Constants -> pi_value = math.pi
+
+5. Random Module:
+The random module is useful for generating random numbers (import random):
+
+# Generate a random integer between 1 and 10 -> random_integer = random.randint(1, 10)
+# Generate a random float between 0 and 1 -> random_float = random.random()
+
+6. Formatting Numbers:
+You can format numbers for display using the format() function or f-strings.
+
+# formatted_number = format(1234567, ",") Output: '1,234,567'
+
+7. Advanced Operations:
+Python supports more advanced operations, such as complex numbers, bitwise operations,
+and modular arithmetic.
+
+# Complex numbers -> complex_result = complex(1, 2) * complex(2, 3)
+# Bitwise operations -> bitwise_and = 5 & 3, bitwise_or = 5 | 3
+# Modulo -> modulo_result = 17 % 5
+
+"""
