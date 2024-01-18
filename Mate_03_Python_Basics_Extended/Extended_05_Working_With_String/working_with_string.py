439b2ee68c4594846f2add6fe5f50a29056828ab
Artem Andriushchenko <link.by.artem@gmail.com>
2024-01-18 02:26:17 +0200

    add working_with_string.py

---
+++ Mate_03_Python_Basics_Extended/Extended_05_Working_With_String/working_with_string.py
@@ -0,0 +1,69 @@
+"""
+Working with strings in Python involves various operations, such as:
+string creation, manipulation, formatting, and searching.
+
+1. Creating Strings:
+Strings in Python can be created using single quotes ('), double quotes ("),
+or triple quotes (''')
+
+# single_quoted = 'Hello, World!'
+# double_quoted = "Python is fun!"
+# triple_quoted = '''This is a multiline string.'''
+
+2. String Concatenation:
+You can concatenate strings using the + operator.
+
+# first_name = "John"
+# last_name = "Doe"
+# full_name = first_name + " " + last_name
+
+3. String Multiplication:
+You can use the * operator to repeat a string.
+
+# greeting = "Hello, " * 3
+
+4. String Indexing and Slicing:
+Access individual characters in a string using indexing, and extract substrings using slicing.
+
+# text = "Python"
+# first_char = text[0]  -> 'P'
+# substring = text[1:4]  -> 'yth'
+
+5.String Methods:
+Python provides a variety of built-in string methods for common operations:
+
+# message = "   Hello, World!   "
+# Remove leading and trailing whitespaces -> rimmed_message = message.strip()
+# Convert to lowercase or uppercase -> lowercase_message = message.lower() or message.upper()
+# Replace substring -> new_message = message.replace("World", "Python")
+# Find substring -> index = message.find("Hello")
+
+6. String Formatting:
+You can format strings using f-strings method (formatted string literals).
+
+# name = "Alice", age = 30
+# formatted_string = f"Name: {name}, Age: {age}"
+
+7. Escape Characters:
+Escape characters are used to represent special characters in a string.
+
+# escaped_string = "This is a \"quote\" and a new line.\nSecond line."
+
+8. String Length:
+Use the len() function to get the length of a string.
+
+# length = len("Hello, World!")
+
+9. Checking Substrings:
+Use the in operator to check if a substring is present in a string.
+
+# contains_python = "Python" in "Hello, Python!"
+
+
+These are just some of the basic string operations in Python.
+Understanding these concepts will help you effectively work with strings and manipulate text data
+in your Python programs.
+
+
+
+"""
